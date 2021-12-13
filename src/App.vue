d5c38878c1861ddb49b023a1b07c0bd69da36271
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-12-13 12:11:11 +0100

    Add macro Header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    
+    <msHeader/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import msHeader from './components/MsHeader.vue'
 
 export default {
   name: 'App',
   components: {
-
+    msHeader,
   },
   data(){
   return {
---
+++ src/components/MsHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <nav>
+            qui inserirò la navbar 
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'msHeader',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    // debug
+    header{
+        height: 300px;
+        background-color: black;
+    }
+    nav{
+        height: 100px;
+        background-color: rgba(199, 199, 199, 0.144);
+        color: white;
+    }
+</style>
