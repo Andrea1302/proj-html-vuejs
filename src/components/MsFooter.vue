cdaec2941c5a1baf0fa3681ab4dc1427b1448077
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-12-13 17:57:00 +0100

    feature: add li static and dinamic in footer

--- src/components/MsFooter.vue
+++ src/components/MsFooter.vue
@@ -2,31 +2,27 @@
     <footer>
         <div class="row justify-content-center">
             <div class="col-lg-3">
-              <h6>
+              <ul> 
                 ABOUT ME
-              </h6>
-              <p>
-                Vivamus suscipit tortor eget felis porttitor volupat. Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat
-              </p>
+                <li>
+                  Vivamus suscipit tortor eget felis porttitor volupat. Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat
+                </li>
+              </ul>
             </div>
             <div class="col-lg-3">
-              <h6>
-                USEFUL LINKS
-              </h6>
               <ul>
+                USEFUL LINKS
                 <li v-for="link,i in infoUsefulLinks" :key="i">
                   <a href="#">
-                    {{link}}
+                    <i class="fas fa-angle-right arrow_right"></i> {{link}}
                   </a>
                 </li>
               </ul>
             </div>
               
             <div class="col-lg-3">
-              <h6>
-                CONTACT INFO 
-              </h6>
               <ul>
+                CONTACT INFO 
                 <li>
                   <a href="#">
                     {{infoContact.address}}
@@ -51,10 +47,41 @@
                   </a>
                 </li>
                 
+                <!-- social  -->
+                <li>
+                  <!-- facebook -->
+                  <a href="#">
+                    <i class="fab fa-facebook-f social"></i>
+                  </a>
+
+                  <!-- twitter  -->
+                  <a href="#">
+                    <i class="fab fa-twitter social"></i>
+                  </a>
+
+                  <!-- Instagram  -->
+                  <a href="#">
+                    <i class="fab fa-instagram social"></i>
+                  </a>
+
+                  <!-- Linkedin -->
+                  <a href="#">
+                    <i class="fab fa-linkedin-in social"></i>
+                  </a>
+                  
+                  <!-- Youtube -->
+                  <a href="#">
+                    <i class="fab fa-youtube social"></i>
+                  </a>
+                </li>
               </ul>
             </div>
         </div>
+        <div id="copyright" class="text-center">
+          &#169; Copyright 2012 - 2020 | Avada Theme by <a href="#">Theme Fusion</a> | All Rights Reserved | Powered by <a href="#">WordPress</a>
+        </div>
     </footer>
+    
 </template>
 
 <script>
@@ -71,9 +98,7 @@ export default {
 <style scoped lang="scss">
     // debug
     footer{
-        // debug
-        height: 500px;
-
+        
         padding: 50px;
         background-image: url(../assets/footer-3.jpg);
         background-size: cover;
@@ -81,7 +106,31 @@ export default {
         ul{
           font-weight: bold;
         } li {
+          font-size: .7rem;
+          margin: 12px 0;
           list-style: none;
+          padding: 0;
+          .arrow_right{
+            margin-right: 10px;
+          }
+          .social{
+            margin-right: 20px;
+            font-size: 15px;
+          }
+          a{
+            text-decoration: none;
+            color: white;
+          }
+        }
+        #copyright{
+          font-size: .7rem;
+          color:#96a4a3;
+          margin-top: 20px;
+          a{
+            color: white;
+            text-decoration: none;
+            font-weight: bold;
+          }
         }
     }
 </style>
