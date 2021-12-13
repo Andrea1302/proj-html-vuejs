7e4ced711f57dea92562d069c93bcf05a62d5b14
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-12-13 11:55:19 +0100

    Create: data for Header and footer dinamic menu

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data(){
+  return {
+    navHeader : 
+      {
+        home : "Home",
+        aboutMe : "About Me",
+        testimonials : "Testimonials",
+        myBlog : "My Blog",
+        meetups : "Meetups",
+        shop : "Shop",
+        contactMe : "Contact Me"
+      },
+    logo : "",
+    // NavFooter
+    textDescription : "Vivamus suscipit tortor eget felis porttitor volupat. Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat",
+    links : ["Latest Books","Upcoming Events","Recent Articles","Business Enquires","Visit My Foundation"],
+    Contact : {
+      address : "63 E. Studebaker Street Bolingbrook, IL 60440",
+      Phone : "(800) 000-0000",
+      mobile : "(800) 000-0000",
+      email : "info@your-company.com",
+    }
+        
+      
+    
   }
 }
+}
+
 </script>
 
 <style lang="scss">
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
